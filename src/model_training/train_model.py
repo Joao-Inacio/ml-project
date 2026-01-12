48187ab191a0a4e1ba6a2eefe6a37749739ee3e0
Joao-Inacio <joaoinacio206@protonmail.com>
2026-01-12 15:39:43 -0300

    mlflow part 1

--- src/model_training/train_model.py
+++ src/model_training/train_model.py
@@ -3,6 +3,7 @@ import logging
 import os
 
 import joblib
+import mlflow
 import numpy as np
 import pandas as pd
 import tensorflow as tf
@@ -39,7 +40,9 @@ def load_params() -> dict[str, float | int]:
     return params["train"]
 
 
-def prepare_data(train_data: pd.DataFrame) -> tuple[pd.DataFrame, np.ndarray, OneHotEncoder]:
+def prepare_data(
+    train_data: pd.DataFrame,
+) -> tuple[pd.DataFrame, np.ndarray, OneHotEncoder]:
     """Prepare data for neural network training by separating features and target, and encoding labels.
 
     Args:
@@ -81,7 +84,7 @@ def create_model(
             Dense(
                 params["hidden_layer_1_neurons"],
                 activation="relu",
-                input_shape=(input_shape,)
+                input_shape=(input_shape,),
             ),
             Dropout(params["dropout_rate"]),
             Dense(
@@ -130,42 +133,61 @@ def train_model(train_data: pd.DataFrame, params: dict[str, int | float]) -> Non
         train_data (pd.DataFrame): Training dataset.
         params (dict[str, int | float]): Model hyperparameters.
     """
-    tf.keras.utils.set_random_seed(params.pop("random_seed"))
-    
-    # Prepare the data
-    X_train, y_train, encoder = prepare_data(train_data)
-    
-    # Create the model
-    model = create_model(
-        input_shape=X_train.shape[1], num_classes=y_train.shape[1], params=params
-    )
+    # Set up Mlflow experiment
+    mlflow.set_experiment("ml_classification")
 
-    # Early stopping to prevent overfitting
-    early_stopping = EarlyStopping(
-        monitor="val_loss", patience=10, restore_best_weights=True
-    )
+    # set up keras autolog
+    mlflow.keras.autolog()
 
-    # Train the model with validation split
-    logger.info("Training model...")
-    history = model.fit(
-        X_train,
-        y_train,
-        validation_split=0.2,
-        epochs=params["epochs"],
-        batch_size=params["batch_size"],
-        callbacks=[early_stopping],
-    )
+    with mlflow.start_run():
+        # Log parameters to MLFlow
+        mlflow.log_params(params)
 
-    save_training_artifacts(model, encoder)
-    
-    # Save training metrics to a file
-    metrics = {
-        metric: float(history.history[metric][-1]) 
-        for metric in history.history
-    }
-    metrics_path = "metrics/training.json"
-    with open(metrics_path, "w") as f:
-        json.dump(metrics, f, indent=2)
+        tf.keras.utils.set_random_seed(params.pop("random_seed"))
+
+        # Log preprocessing artifacts
+        mlflow.log_artifact("artifacts/[features]_mean_imputer.joblib")
+        mlflow.log_artifact("artifacts/[features]_scaler.joblib")
+
+        # Prepare the data
+        X_train, y_train, encoder = prepare_data(train_data)
+
+        # Create the model
+        model = create_model(
+            input_shape=X_train.shape[1], num_classes=y_train.shape[1], params=params
+        )
+
+        # Early stopping to prevent overfitting
+        early_stopping = EarlyStopping(
+            monitor="val_loss", patience=10, restore_best_weights=True
+        )
+
+        # Train the model with validation split
+        logger.info("Training model...")
+        history = model.fit(
+            X_train,
+            y_train,
+            validation_split=0.2,
+            epochs=params["epochs"],
+            batch_size=params["batch_size"],
+            callbacks=[early_stopping],
+        )
+
+        save_training_artifacts(model, encoder)
+
+        # log the encoder
+        mlflow.log_artifact("artifacts/[target]_one_hot_encoder.joblib")
+
+        # Save training metrics to a file
+        metrics = {
+            metric: float(history.history[metric][-1]) for metric in history.history
+        }
+        metrics_path = "metrics/training.json"
+        with open(metrics_path, "w") as f:
+            json.dump(metrics, f, indent=2)
+
+        # Log metrics to MLFlow
+        mlflow.log_metrics(metrics)
 
 
 def main() -> None:
