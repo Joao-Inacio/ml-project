d1da24be427a9f4d5a50cbadecd1f6acc808210a
Joao-Inacio <joaoinacio206@protonmail.com>
2026-01-14 16:22:21 -0300

    mlflow part 5

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,13 @@
 import logging
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # Configure the logging strategy
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
-    handlers=[
-        logging.StreamHandler()
-    ]
+    handlers=[logging.StreamHandler()],
 )
